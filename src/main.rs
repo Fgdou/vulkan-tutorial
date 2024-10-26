75d6b6a40a4f0b930bb9487b98bb287ee4a3da33
Fgdou <fabigoardou@gmail.com>
2024-10-26 16:07:58 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -183,4 +183,5 @@ impl App {
 /// The Vulkan handles and associated properties used by our Vulkan app.
 #[derive(Clone, Debug, Default)]
 struct AppData {
+    messenger: vk::DebugUtilsMessengerEXT,
 }
